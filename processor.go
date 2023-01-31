983005f5f796f651e047febe2de2e830c728552c
Marc-Antoine <magiguere@mirego.com>
2023-01-31 17:18:31 -0500

    unh, neat. I didn't know that

--- processor.go
+++ processor.go
@@ -12,7 +12,7 @@ func (p *MapProcessor[T, R]) execute(value T) R {
 	return p.block(value)
 }
 
-func Map[T any, R any](p *Publisher[T], block func(T) R) *Publisher[R] {
+func Map[T, R any](p *Publisher[T], block func(T) R) *Publisher[R] {
 	new_pub := Publisher[R]{}
 	p.Subscribe(func(t T) {
 		new_pub.Publish(block(t))
